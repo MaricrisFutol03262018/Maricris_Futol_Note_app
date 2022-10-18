bf6b7f66ad1c7541d8ddebd38ba05351281ca137
Maricris_Futol <futolmaricris@gmail.com>
2022-10-18 15:32:00 +0700

    add. js and present. js updated

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const write = require ('./write');
-const cmd = process.argv[2]
-// write (cmd);
+const add = require('./add')
+const present = require('./present')
+const read = require('./read')
+// const present = require('./present')
+// get user input
+const cmd = process.argv
 
-const read = require ('./read');
-console.log(read())
+var note = {}
+
+if(cmd[2] == 'add') {
+    // Build Object
+    note = {
+        id: cmd[3],
+        title: cmd[4],
+        body: cmd[5]
+    } 
+    // Get old note value
+    var oldNote = read()
+    // Add note to note.txt
+    add(note,oldNote)
+}
+
+if (cmd[2] == 'read') {
+    present(read())
+}
