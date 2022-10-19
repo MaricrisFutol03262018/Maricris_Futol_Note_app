759662910a945bdec7f3deb5369cf6baee0e73bc
Maricris_Futol <futolmaricris@gmail.com>
2022-10-19 10:35:41 +0700

    update.js added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const add = require('./add')
 const present = require('./present')
 const read = require('./read')
+const update = require('./update')
 // const present = require('./present')
 // get user input
 const cmd = process.argv
@@ -31,4 +32,15 @@ if (cmd[2] == 'delete') {
     let del = require('./del')
     del (id,oldNote)
     console.log(read())
+}
+if (cmd[2] == 'update') {
+    let note = {
+        id : cmd[3],
+        title: cmd[4],
+        body: cmd [5]
+    }
+    var oldNote = read()
+    let update = require('./update')
+
+    update(note,oldNote)
 }
